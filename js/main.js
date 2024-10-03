5e3396ddd303add4f3adeaea9e44b1bc6137cec6
jtong0 <168995571+jtong0@users.noreply.github.com>
2024-10-02 21:52:26 -0600

    first draft

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
 // Enter your code below.
+console.log ("js file linked");
+
+let divJavascriptResources = document.getElementsByClassName("javascript-resources");
+let btnJavascriptResources = document.getElementById("show-resources");
+btnJavascriptResources.addEventListener('click', function()
+{
+    let dNone = document.querySelector('.d-none');
+   // if (dNone){
+    dNone.classList.remove('d-none');
+    // } 
+});
+
+let linkContainer = document.querySelector('.javascript-resources');
+linkContainer.addEventListener('mouseover',function(event)
+{
+    if (event.target.tagName === 'A')
+    {   
+        console.log(event.target.innerText);
+        let parentDiv = event.target.parentElement;
+        parentDiv.classList.add('fw-bold');
+    }
+
+});
+
+linkContainer.addEventListener('mouseout',function(event)
+{
+    if (event.target.tagName === 'A')
+    {   
+        console.log(event.target.innerText);
+        let parentDiv = event.target.parentElement;
+        parentDiv.classList.remove('fw-bold');
+    }
+
+});
+
+linkContainer.addEventListener('mouseout',function(event)
+{
+    if (event.target.tagName === 'A')
+    {   
+        console.log(event.target.innerText);
+        let parentDiv = event.target.parentElement;
+        parentDiv.classList.add('fst-italic');
+    }
+
+});
